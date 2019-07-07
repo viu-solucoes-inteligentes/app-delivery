2a3d60f0ac8d429f834b6921f727256c06f3f26b
Carlos Clayton <carlos.clayton@gmail.com>
2019-07-07 16:31:33 -0300

    Updating Wellcome version

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Forked version 5.7
+                    Laravel version 5.7.1
                 </div>
 
                 <div class="links">
