59a52e0e066b7b2636a950fe5fb14c9d92c5586b
Carlos Clayton <carlos.clayton@gmail.com>
2019-07-07 19:33:07 -0300

    Updating Wellcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel version 5.7.1
+                    Laravel 5
                 </div>
 
                 <div class="links">
